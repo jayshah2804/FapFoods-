c4b00deddd49f7740fa831351b4ca847b371c87e
jayshah28 <jayshah9791@example.com>
2022-11-01 15:56:41 +0530

    auto route added

--- src/Components/Dashboard/Main.js
+++ src/Components/Dashboard/Main.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import classes from './Main.module.css';
 import Chart from 'react-apexcharts';
 import photo from '../../Assets/admin.jpg';
+import startPoint from "../../Assets/Pin_icon_green50.png";
+import studentDummyImage from "../../Assets/new_student_marker.png";
 
 const DUMMY_DATA = [
   {
@@ -91,8 +93,51 @@ const initial = {
   },
 };
 
+const RIDER_NAMES = [
+  {
+    name: "Jay Shah",
+    status: false
+  },
+  {
+    name: "Dev Shah",
+    status: false
+  },
+  {
+    name: "Het Desai",
+    status: false
+  },
+  {
+    name: "Roshan Patel",
+    status: false
+  },
+  {
+    name: "Nihar Gupte",
+    status: false
+  },
+  {
+    name: "Vinay Joshi",
+    status: false
+  },
+];
+
+const tourStops = [
+  [{ lat: 23.037569650831212, lng: 72.55877665822754 }],
+  [{ lat: 23.037569650831211, lng: 72.55877665822752 }],
+  [{ lat: 23.036569650831218, lng: 72.55877665822800 }],
+  [{ lat: 23.035569650831218, lng: 72.56077665822800 }],
+  [{ lat: 23.03489120423814, lng: 72.56658725087891 }],
+  [{ lat: 23.03248207530169, lng: 72.56562165563355 }],
+  [{ lat: 23.032583894197987, lng: 72.56023406982422 }],
+  // [{ lat: 23.032583894197997, lng: 72.56023406982430 }]
+];
+
+const flightPlanCoordinates = [
+  { lat: tourStops[0][0].lat, lng: tourStops[0][0].lng },
+];
+
 const Main = () => {
-  const [options, setOptions] = useState(initial);
+  // const [options, setOptions] = useState(initial);
+  const [isRender, setIsRender] = useState();
 
   const script = document.createElement('script');
   script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAq88vEj-mQ9idalgeP1IuvulowkkFA-Nk&callback=myInitMap&libraries=places&v=weekly";
@@ -100,14 +145,112 @@ const Main = () => {
   document.body.appendChild(script);
 
   function myInitMap() {
-    var map = new window.google.maps.Map(document.getElementById("map-modal"), {
-      center: { lat: 23.0225, lng: 72.5714 },
-      zoom: 11,
-      mapTypeControl: false,
+    // var map = new window.google.maps.Map(document.getElementById("map-modal"), {
+    //   center: { lat: 23.0225, lng: 72.5714 },
+    //   zoom: 11,
+    //   mapTypeControl: false,
+    // });
+    const map = new window.google.maps.Map(document.getElementById("map-modal"), {
+      zoom: 15,
+      center: { lat: 23.03489120423814, lng: 72.56658725087891 },
+    });
+
+    const infoWindow = new window.google.maps.InfoWindow();
+    let icon;
+    let myTitle;
+
+    function distance(lat1,
+      lat2, lon1, lon2) {
+
+      // The math module contains a function
+      // named toRadians which converts from
+      // degrees to radians.
+      lon1 = lon1 * Math.PI / 180;
+      lon2 = lon2 * Math.PI / 180;
+      lat1 = lat1 * Math.PI / 180;
+      lat2 = lat2 * Math.PI / 180;
+
+      // Haversine formula
+      let dlon = lon2 - lon1;
+      let dlat = lat2 - lat1;
+      let a = Math.pow(Math.sin(dlat / 2), 2)
+        + Math.cos(lat1) * Math.cos(lat2)
+        * Math.pow(Math.sin(dlon / 2), 2);
+
+      let c = 2 * Math.asin(Math.sqrt(a));
+
+      // Radius of earth in kilometers. Use 3956
+      // for miles
+      let r = 6371;
+
+      // calculate the result
+      return (c * r * 1000);
+    }
+
+    const flightPath = new window.google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: "blue",
+      strokeOpacity: 0.9,
+      strokeWeight: 6,
+    });
+
+    flightPath.setMap(map);
+
+    const assignButtonClickHandler = (e) => {
+      console.log(e.target.parentElement.id);
+      RIDER_NAMES[e.target.parentElement.id - 1].status = true;
+      console.log(RIDER_NAMES);
+      let obj = {};
+      obj.lat = tourStops[e.target.parentElement.id][0].lat;
+      obj.lng = tourStops[e.target.parentElement.id][0].lng;
+      flightPlanCoordinates.push(obj);
+      console.log("this", flightPlanCoordinates);
+      setIsRender(prev => !prev);
+    }
+
+    tourStops.forEach(([position], i) => {
+      if (i === 0) {
+        icon = startPoint;
+        myTitle = `<div><h3>Vishala Circle</h3></div>`;
+      }
+      else {
+        icon = studentDummyImage;
+        if (RIDER_NAMES[i - 1].status)
+          myTitle = `<div id="infowindow-container" ><h3>${RIDER_NAMES[i - 1].name}</h3><p>Assigned</div>`;
+        else
+          myTitle = `<div id="infowindow-container" ><h3>${RIDER_NAMES[i - 1].name}</h3><div id=${i}><button id='myButton'>Assign rider</button></div></div>`;
+        // myTitle = `<div id="infowindow-container" ><h3>${RIDER_NAMES[i - 1].name}</h3></p><button onclick="(function func(e) {
+        // }())">Assign rider</button></div>`;
+      }
+
+      let distanceBetweenPoints = distance(tourStops[0][0].lat, position.lat, tourStops[0][0].lng, position.lng);
+      let marker;
+      // if (distanceBetweenPoints <= 500) {
+        marker = new window.google.maps.Marker({
+          position,
+          map,
+          myTitle,
+          icon,
+          optimized: false,
+        });
+      // }
+
+      marker.addListener("mouseover", () => {
+        // infoWindow.close();
+        infoWindow.setContent(marker.myTitle);
+        infoWindow.open(marker.getMap(), marker);
+        infoWindow.open(
+          setTimeout(() => {
+            document.getElementById("myButton").addEventListener('click', assignButtonClickHandler)
+          })
+        );
+      });
+
     });
 
     let myInt = setInterval(() => {
-      if(document.getElementsByClassName("gm-control-active")[0]){
+      if (document.getElementsByClassName("gm-control-active")[0]) {
         document.getElementsByClassName("gm-control-active")[0].style.marginTop = "40px";
       }
       if (document.getElementsByClassName("gm-svpc")[0]) {
@@ -116,6 +259,7 @@ const Main = () => {
       }
     })
   }
+
   window.myInitMap = myInitMap;
 
   return (
