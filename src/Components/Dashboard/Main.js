9ba4c34380de66d286f44db14c1753c69336b090
jayshah28 <jayshah9791@example.com>
2022-11-01 17:10:52 +0530

    stroke changes

--- src/Components/Dashboard/Main.js
+++ src/Components/Dashboard/Main.js
@@ -121,18 +121,17 @@ const RIDER_NAMES = [
 ];
 
 const tourStops = [
-  [{ lat: 23.037569650831212, lng: 72.55877665822754 }],
-  [{ lat: 23.037569650831211, lng: 72.55877665822752 }],
-  [{ lat: 23.036569650831218, lng: 72.55877665822800 }],
-  [{ lat: 23.035569650831218, lng: 72.56077665822800 }],
-  [{ lat: 23.03489120423814, lng: 72.56658725087891 }],
-  [{ lat: 23.03248207530169, lng: 72.56562165563355 }],
-  [{ lat: 23.032583894197987, lng: 72.56023406982422 }],
-  // [{ lat: 23.032583894197997, lng: 72.56023406982430 }]
+  { lat: 23.037569650831212, lng: 72.55877665822754 },
+  { lat: 23.036569650831218, lng: 72.55877665822800 },
+  { lat: 23.037569650831211, lng: 72.55877665822752 },
+  { lat: 23.035569650831218, lng: 72.56077665822800 },
+  { lat: 23.03489120423814, lng: 72.56658725087891 },
+  { lat: 23.03248207530169, lng: 72.56562165563355 },
+  { lat: 23.032583894197987, lng: 72.56023406982422 },
 ];
 
 const flightPlanCoordinates = [
-  { lat: tourStops[0][0].lat, lng: tourStops[0][0].lng },
+  { lat: tourStops[0].lat, lng: tourStops[0].lng },
 ];
 
 const Main = () => {
@@ -153,48 +152,32 @@ const Main = () => {
     const map = new window.google.maps.Map(document.getElementById("map-modal"), {
       zoom: 15,
       center: { lat: 23.03489120423814, lng: 72.56658725087891 },
+      disableDefaultUI: true,
+      fullscreenControl: true,
+      zoomControl: true
     });
 
     const infoWindow = new window.google.maps.InfoWindow();
     let icon;
     let myTitle;
 
-    function distance(lat1,
-      lat2, lon1, lon2) {
-
-      // The math module contains a function
-      // named toRadians which converts from
-      // degrees to radians.
-      lon1 = lon1 * Math.PI / 180;
-      lon2 = lon2 * Math.PI / 180;
-      lat1 = lat1 * Math.PI / 180;
-      lat2 = lat2 * Math.PI / 180;
-
-      // Haversine formula
-      let dlon = lon2 - lon1;
-      let dlat = lat2 - lat1;
-      let a = Math.pow(Math.sin(dlat / 2), 2)
-        + Math.cos(lat1) * Math.cos(lat2)
-        * Math.pow(Math.sin(dlon / 2), 2);
-
-      let c = 2 * Math.asin(Math.sqrt(a));
-
-      // Radius of earth in kilometers. Use 3956
-      // for miles
-      let r = 6371;
-
-      // calculate the result
-      return (c * r * 1000);
-    }
+    const flightPath1 = new window.google.maps.Polyline({
+      path: flightPlanCoordinates,
+      geodesic: true,
+      strokeColor: "black",
+      strokeOpacity: 1.0,
+      strokeWeight: 7,
+    });
 
     const flightPath = new window.google.maps.Polyline({
       path: flightPlanCoordinates,
       geodesic: true,
-      strokeColor: "blue",
-      strokeOpacity: 0.9,
-      strokeWeight: 6,
+      strokeColor: "rgba(34, 137, 203, 255)",
+      strokeOpacity: 1.0,
+      strokeWeight: 5,
     });
 
+    flightPath1.setMap(map);
     flightPath.setMap(map);
 
     const assignButtonClickHandler = (e) => {
@@ -202,14 +185,13 @@ const Main = () => {
       RIDER_NAMES[e.target.parentElement.id - 1].status = true;
       console.log(RIDER_NAMES);
       let obj = {};
-      obj.lat = tourStops[e.target.parentElement.id][0].lat;
-      obj.lng = tourStops[e.target.parentElement.id][0].lng;
+      obj.lat = tourStops[e.target.parentElement.id].lat;
+      obj.lng = tourStops[e.target.parentElement.id].lng;
       flightPlanCoordinates.push(obj);
-      console.log("this", flightPlanCoordinates);
       setIsRender(prev => !prev);
     }
 
-    tourStops.forEach(([position], i) => {
+    tourStops.forEach((position, i) => {
       if (i === 0) {
         icon = startPoint;
         myTitle = `<div><h3>Vishala Circle</h3></div>`;
@@ -220,21 +202,15 @@ const Main = () => {
           myTitle = `<div id="infowindow-container" ><h3>${RIDER_NAMES[i - 1].name}</h3><p>Assigned</div>`;
         else
           myTitle = `<div id="infowindow-container" ><h3>${RIDER_NAMES[i - 1].name}</h3><div id=${i}><button id='myButton'>Assign rider</button></div></div>`;
-        // myTitle = `<div id="infowindow-container" ><h3>${RIDER_NAMES[i - 1].name}</h3></p><button onclick="(function func(e) {
-        // }())">Assign rider</button></div>`;
       }
 
-      let distanceBetweenPoints = distance(tourStops[0][0].lat, position.lat, tourStops[0][0].lng, position.lng);
-      let marker;
-      // if (distanceBetweenPoints <= 500) {
-        marker = new window.google.maps.Marker({
-          position,
-          map,
-          myTitle,
-          icon,
-          optimized: false,
-        });
-      // }
+      const marker = new window.google.maps.Marker({
+        position,
+        map,
+        myTitle,
+        icon,
+        optimized: false,
+      });
 
       marker.addListener("mouseover", () => {
         // infoWindow.close();
@@ -246,17 +222,17 @@ const Main = () => {
           })
         );
       });
-
     });
 
     let myInt = setInterval(() => {
       if (document.getElementsByClassName("gm-control-active")[0]) {
         document.getElementsByClassName("gm-control-active")[0].style.marginTop = "40px";
-      }
-      if (document.getElementsByClassName("gm-svpc")[0]) {
-        document.getElementsByClassName("gm-svpc")[0].style.display = "none";
         clearInterval(myInt);
       }
+      // if (document.getElementsByClassName("gm-svpc")[0]) {
+      //   document.getElementsByClassName("gm-svpc")[0].style.display = "none";
+      //   clearInterval(myInt);
+      // }
     })
   }
 
@@ -320,3 +296,31 @@ const Main = () => {
 }
 
 export default Main;
+
+// function distance(lat1,
+    //   lat2, lon1, lon2) {
+
+    //   // The math module contains a function
+    //   // named toRadians which converts from
+    //   // degrees to radians.
+    //   lon1 = lon1 * Math.PI / 180;
+    //   lon2 = lon2 * Math.PI / 180;
+    //   lat1 = lat1 * Math.PI / 180;
+    //   lat2 = lat2 * Math.PI / 180;
+
+    //   // Haversine formula
+    //   let dlon = lon2 - lon1;
+    //   let dlat = lat2 - lat1;
+    //   let a = Math.pow(Math.sin(dlat / 2), 2)
+    //     + Math.cos(lat1) * Math.cos(lat2)
+    //     * Math.pow(Math.sin(dlon / 2), 2);
+
+    //   let c = 2 * Math.asin(Math.sqrt(a));
+
+    //   // Radius of earth in kilometers. Use 3956
+    //   // for miles
+    //   let r = 6371;
+
+    //   // calculate the result
+    //   return (c * r * 1000);
+    // }
