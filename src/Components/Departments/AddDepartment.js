18fd6129b00ef4e13aa79e7e65419e9f4ef4254c
jayshah28 <jayshah9791@example.com>
2022-11-16 12:16:23 +0530

    add department change

--- src/Components/Departments/AddDepartment.js
+++ src/Components/Departments/AddDepartment.js
@@ -1,9 +1,59 @@
 import React from 'react';
 import "./AddDepartment.css";
 
+var showVehicle = true;
+var showRide = true;
+let allow_inter_country = false;
 const AddDepartment = () => {
+    function showVehicleCheckboxes() {
+        var checkboxes = document.getElementsByClassName("multipleSelection")[0].children[1];
+
+        if (showVehicle) {
+            checkboxes.style.display = "block";
+            showVehicle = false;
+        } else {
+            let length = document.getElementsByClassName("multipleSelection")[0].children[1].children.length;
+            let arr = [];
+            for (let i = 0; i < length; i++) {
+                if (document.getElementsByClassName("multipleSelection")[0].children[1].children[i]?.checked)
+                    arr.push(document.getElementsByClassName("multipleSelection")[0].children[1].children[i].value)
+            }
+            if (arr.toString())
+                document.getElementsByTagName("select")[0].children[0].innerText = arr.toString();
+            else
+                document.getElementsByTagName("select")[0].children[0].innerText = "Select";
+            checkboxes.style.display = "none";
+            showVehicle = true;
+        }
+    }
+    function showRideCheckboxes() {
+        var checkboxes = document.getElementsByClassName("multipleSelection")[1].children[1];
+
+        if (showRide) {
+            checkboxes.style.display = "block";
+            showRide = false;
+        } else {
+            let length = document.getElementsByClassName("multipleSelection")[1].children[1].children.length;
+            let arr = [];
+            for (let i = 0; i < length; i++) {
+                if (document.getElementsByClassName("multipleSelection")[1].children[1].children[i]?.checked)
+                    arr.push(document.getElementsByClassName("multipleSelection")[1].children[1].children[i].value)
+            }
+            if (arr.toString())
+                document.getElementsByTagName("select")[1].children[0].innerText = arr.toString();
+            else
+                document.getElementsByTagName("select")[1].children[0].innerText = "Select";
+            checkboxes.style.display = "none";
+            showRide = true;
+        }
+    }
+
+    const interCountryChangeHandler = (e) => {
+        e.target.checked ? (allow_inter_country = "true") : (allow_inter_country = "false");
+        // console.log(allow_inter_country);
+    }
     return (
-        <div className='add-department-container'>
+        <div className='add-department-container' id='add-department'>
             <div>
                 <h3>ADD NEW DEPARTMENT</h3>
             </div>
@@ -12,29 +62,25 @@ const AddDepartment = () => {
                     <main>
                         <header style={{ display: "flex", justifyContent: "space-between" }}>
                             <span>Allow Inter Country</span>
-                            <div class="container">
-                                <label class="switch" for="checkbox">
-                                    <input type="checkbox" id="checkbox" className='first' />
-                                    <div class="slider1 round"></div>
-                                </label>
-                            </div>
+                            <label class="switch" for="checkbox">
+                                <input type="checkbox" id="checkbox" className='first' onChange={interCountryChangeHandler} />
+                                <div class="slider round"></div>
+                            </label>
                         </header>
-                        <div>This option enables one to take Corporate Trips in different countries</div>
+                        <div className='text'>This option enables one to take Corporate Trips in different countries</div>
                     </main>
                     <footer></footer>
                 </div>
                 <div>
                     <main>
-                    <header style={{ display: "flex", justifyContent: "space-between" }}>
+                        <header style={{ display: "flex", justifyContent: "space-between" }}>
                             <span>Lock Vehicle Type</span>
-                            <div class="container">
-                                <label class="switch" for="checkbox">
+                            {/* <label class="switch2" for="checkbox">
                                     <input type="checkbox" id="checkbox" className='second' />
                                     <div class="slider2 round"></div>
-                                </label>
-                            </div>
+                                </label> */}
                         </header>
-                        <div>If set to 'YES' this will restrict all staff in this corporate from changing the approved vehicle type when requesting for a trip</div>
+                        <div className='text'>If set to 'YES' this will restrict all staff in this corporate from changing the approved vehicle type when requesting for a trip</div>
                     </main>
                     <footer></footer>
                 </div>
@@ -43,13 +89,24 @@ const AddDepartment = () => {
                         <header>
                             <span>Preferred Vehicle Categories</span>
                         </header>
-                        <select>
-                            <option disabled selected>Select</option>
-                            <option>Basic</option>
-                            <option>Comfort</option>
-                            <option>Comfort plus</option>
-                        </select>
-                        <div>Select the vehicle categories you wish your company to use</div>
+                        <div class="multipleSelection">
+                            <div class="selectBox" onClick={showVehicleCheckboxes}>
+                                <select>
+                                    <option>Select</option>
+                                </select>
+                                <div class="overSelect"></div>
+                            </div>
+
+                            <div id="checkBoxes">
+                                <input type="checkbox" value="Basic" id="first" /><span>Basic</span>
+                                <br />
+                                <input type="checkbox" value="Comfort" id="second" /><span>Comfort</span>
+                                <br />
+                                <input type="checkbox" value="Comfort Plus" id="third" /><span>Comfort Plus</span>
+                                <br />
+                            </div>
+                        </div>
+                        <div className='text'>Select the vehicle categories you wish your company to use</div>
                     </main>
                     <footer></footer>
                 </div>
@@ -58,19 +115,29 @@ const AddDepartment = () => {
                         <header>
                             <span>Enabled Services</span>
                         </header>
-                        <select>
-                            <option>Select</option>
-                            <option>Ride</option>
-                            <option>Food</option>
-                        </select>
-                        <div>Select the Services you wish your company to use</div>
+                        <div class="multipleSelection">
+                            <div class="selectBox" onClick={showRideCheckboxes}>
+                                <select>
+                                    <option>Select</option>
+                                </select>
+                                <div class="overSelect"></div>
+                            </div>
+
+                            <div id="checkBoxes">
+                                <input type="checkbox" value="Ride" id="first" /><span>Ride</span>
+                                <br />
+                                <input type="checkbox" value="Food" id="second" /><span>Food</span>
+                                <br />
+                            </div>
+                        </div>
+                        <div className='text'>Select the Services you wish your company to use</div>
                     </main>
                     <footer></footer>
                 </div>
             </div>
             <div className='create-department'>
                 <header>Create Department</header>
-                <div style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
+                <div style={{ display: "flex", alignItems: "center", margin: "0px 40px", gap: "100px" }}>
                     <input type="text" placeholder='Department Name' />
                     <input type="text" placeholder='Admin Name' />
                     <input type="email" placeholder='Admin Email' />
